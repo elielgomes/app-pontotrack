d95addb1609c8eb8b4bc23b5012347304472e944
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-22 15:05:48 -0300

    :sparkles: feat(schema): Adiciona schema para credentials login

---
+++ src/schemas/credentials.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const credentialsSchema = z.object({
+  email: z.string().email({
+    message: "Preencha o e-mail corretamente",
+  }),
+  password: z.string().min(6, {
+    message: "Preencha a senha corretamente",
+  }),
+});
