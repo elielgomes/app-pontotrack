4af1bd3cb7050bb584ad54d460f752e30acfcb26
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-23 10:20:50 -0300

    :sparkles: feat(user): Implementa user provider no dashboard layout

---
+++ src/contexts/user-provider.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { user as userService } from "@/services/user";
+import { User } from "@/services/user/types";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import React, { createContext } from "react";
+import { toast } from "sonner";
+import { userKeys } from "@/query/query-key";
+
+export interface UserContextData {
+  isLoading: boolean;
+  user: Omit<User, "password"> | undefined;
+}
+
+export const UserContext = createContext<UserContextData | undefined>(
+  undefined
+);
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  const {
+    isLoading,
+    data: user,
+    error,
+  } = useSuspenseQuery({
+    queryKey: userKeys.me,
+    queryFn: userService.me,
+    refetchOnWindowFocus: false,
+  });
+
+  if (error) {
+    toast.error("Erro ao buscar usuário");
+  }
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        isLoading,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
