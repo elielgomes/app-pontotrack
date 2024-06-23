eff2a21431b40df0cdf79fa033b88c99af94f439
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-23 18:15:22 -0300

    :sparkles: feat: Infere user na tipagem de retorno de login

--- src/services/auth/types/index.ts
+++ src/services/auth/types/index.ts
@@ -1,6 +1,8 @@
 import { CredentialsSchema } from "@/schemas/credentials";
+import { User } from "@/services/user/types";
 
 export interface LoginInput extends CredentialsSchema {}
 export interface LoginOutput {
   access_token: string;
+  user: Omit<User, "password">;
 }
