bcdc53feda769fad0eff8777e920de048f3004f2
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-23 18:13:16 -0300

    :sparkles: feat: Cria componente para conteudo central da dashboard

---
+++ src/components/layout/dashboard/main/index.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { cn } from "@/lib/tw-merge";
+
+interface ContentBaseProps<T> extends React.HTMLAttributes<T> {
+  children?: React.ReactNode;
+}
+
+/**
+ * @example
+ *
+ * ```tsx
+ * <MainWrapper>
+ * 	<MainContentHeader>
+ * 		<MainContentTitle>Dashboard</MainContentTitle>
+ * 	</MainContentHeader>
+ * 	<MainContentWrapper>{children}</MainContentWrapper>
+ * </MainWrapper>
+ *	```
+ */
+export const MainWrapper: React.FC<ContentBaseProps<HTMLDivElement>> = ({
+  children,
+  className,
+  ...props
+}) => {
+  return (
+    <main
+      className={cn(
+        "flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6",
+        className
+      )}
+    >
+      {children}
+    </main>
+  );
+};
+
+export const MainContentWrapper: React.FC<ContentBaseProps<HTMLDivElement>> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <div
+      {...props}
+      className={cn(
+        "flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm",
+        props.className
+      )}
+      x-chunk="dashboard-chunk-1"
+    >
+      {children}
+    </div>
+  );
+};
+
+export const MainContentHeader: React.FC<ContentBaseProps<HTMLDivElement>> = ({
+  children,
+  className,
+  ...props
+}) => {
+  return (
+    <div {...props} className={cn("flex items-center", className)}>
+      {children}
+    </div>
+  );
+};
+
+export const MainContentTitle: React.FC<
+  ContentBaseProps<HTMLHeadingElement>
+> = ({ children, className, ...props }) => {
+  return (
+    <h1
+      {...props}
+      className={cn("text-lg font-semibold md:text-2xl", className)}
+    >
+      {children}
+    </h1>
+  );
+};
