1ee0aa36edbb1efc9196fa98721931bacb88ae2f
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-23 20:39:13 -0300

    :lipstick: feat: Limita largura do header com container

--- src/components/layout/dashboard/header/index.tsx
+++ src/components/layout/dashboard/header/index.tsx
@@ -16,56 +16,66 @@ import { Button } from "@/components/ui/button";
 import { useActivePath } from "@/hooks/use-active-path";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { sidebarLinks } from "@/components/layout/dashboard/links";
+import { ToggleTheme } from "@/components/toggle-theme";
 
 export const Header: React.FC = () => {
   const { isActive } = useActivePath();
   return (
-    <header className="flex justify-between md:justify-end h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
-            <Menu className="h-5 w-5" />
-            <span className="sr-only">Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left" className="flex flex-col">
-          <nav className="grid gap-2 text-lg font-medium space-y-4">
-            <span className="flex items-center gap-2 text-lg font-semibold">
-              <FullLogo />
-              <span className="sr-only">AppTrack</span>
-            </span>
-            <div className="flex flex-col gap-2">
-              {sidebarLinks.map((item) => (
-                <Link
-                  key={item.title}
-                  href={item.href}
-                  data-active={isActive(item.href)}
-                  className="data-[active=true]:bg-muted data-[active=true]:text-primary mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                >
-                  <item.icon className="h-5 w-5" />
-                  {item.title}
-                </Link>
-              ))}
-            </div>
-          </nav>
-        </SheetContent>
-      </Sheet>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="secondary" size="icon" className="rounded-full">
-            <CircleUser className="h-5 w-5" />
-            <span className="sr-only">Toggle user menu</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>Minha Conta</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Configurações</DropdownMenuItem>
-          <DropdownMenuItem>Suporte</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Sair</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+    <header className="h-14 border-b bg-muted/40 lg:h-[60px]">
+      <div className="container flex justify-between md:justify-end h-14 items-center gap-4 border-b lg:h-[60px] bg-slate-50/20">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
+            >
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="flex flex-col">
+            <nav className="grid gap-2 text-lg font-medium space-y-4">
+              <span className="flex items-center gap-2 text-lg font-semibold">
+                <FullLogo />
+                <span className="sr-only">AppTrack</span>
+              </span>
+              <div className="flex flex-col gap-2">
+                {sidebarLinks.map((item) => (
+                  <Link
+                    key={item.title}
+                    href={item.href}
+                    data-active={isActive(item.href)}
+                    className="data-[active=true]:bg-muted data-[active=true]:text-primary mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                  >
+                    <item.icon className="h-5 w-5" />
+                    {item.title}
+                  </Link>
+                ))}
+              </div>
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <div className="flex gap-4">
+          <ToggleTheme />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Minha Conta</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Configurações</DropdownMenuItem>
+              <DropdownMenuItem>Suporte</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Sair</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
     </header>
   );
 };
