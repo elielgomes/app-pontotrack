7771a36f46150f445d68ed680ba37e692bf137f3
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-24 20:55:56 -0300

    :sparkles: feat: Cria metodo de registro de usuario para objeto service de auth

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,9 +1,25 @@
 import { api } from "@/lib/axios/api";
-import type { LoginInput, LoginOutput } from "@/services/auth/types/";
+import type {
+  LoginInput,
+  LoginOutput,
+  RegisterInput,
+  RegisterOutput,
+} from "@/services/auth/types/";
 
 export const auth = {
-  login: async (param: LoginInput) => {
-    const response = await api.post<LoginOutput>("/auth/login", param);
+  login: async ({ email, password }: LoginInput) => {
+    const response = await api.post<LoginOutput>("/auth/login", {
+      email,
+      password,
+    });
+    return response.data;
+  },
+  register: async ({ email, name, password }: RegisterInput) => {
+    const response = await api.post<RegisterOutput>("/auth/register", {
+      email,
+      name,
+      password,
+    });
     return response.data;
   },
 };
