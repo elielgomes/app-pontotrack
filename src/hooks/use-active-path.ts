6b956495410c75e793d90d25708081b57c1a65ca
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-25 21:49:41 -0300

    :sparkles: feat(hook): Implementa metodo novo para match de paths do app

--- src/hooks/use-active-path.ts
+++ src/hooks/use-active-path.ts
@@ -6,10 +6,15 @@ export const useActivePath = () => {
   const pathname = usePathname();
 
   const isActive = (href: string) => {
+    return pathname.includes(href);
+  };
+
+  const isActiveEquals = (href: string) => {
     return pathname === href;
   };
 
   return {
     isActive,
+    isActiveEquals,
   };
 };
