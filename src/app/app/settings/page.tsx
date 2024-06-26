d6088b2f51154173dae8d0becf882ba47a8de039
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-25 21:55:18 -0300

    :sparkles: feat: Implementa formulario de alteracao de senha na pagina de configuracoes

--- src/app/app/settings/page.tsx
+++ src/app/app/settings/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { AccountForm } from "@/app/app/settings/_components/forms/account";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -10,23 +11,40 @@ import {
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { NextPage } from "next";
+import { DeleteAccountForm } from "@/app/app/settings/_components/forms/delete-account";
 
 const SettingsPage: NextPage = () => {
   return (
-    <Card className="flex-1">
-      <CardHeader className="space-y-6">
-        <div>
-          <CardTitle className="text-lg">Minha conta</CardTitle>
-          <CardDescription>
-            Mantenha sempre suas informações pessoais atualizadas
-          </CardDescription>
-        </div>
-        <Separator />
-      </CardHeader>
-      <CardContent>
-        <AccountForm />
-      </CardContent>
-    </Card>
+    <div className="flex flex-col flex-1 gap-6">
+      <Card className="flex-1">
+        <CardHeader className="space-y-6">
+          <div>
+            <CardTitle className="text-lg">Minha conta</CardTitle>
+            <CardDescription>
+              Mantenha sempre suas informações pessoais atualizadas
+            </CardDescription>
+          </div>
+          <Separator />
+        </CardHeader>
+        <CardContent>
+          <AccountForm />
+        </CardContent>
+      </Card>
+      <Card className="flex-1">
+        <CardHeader className="space-y-6">
+          <div>
+            <CardTitle className="text-lg">Excluir conta</CardTitle>
+            <CardDescription>
+              Essa ação é irreversível, tenha certeza do que está fazendo
+            </CardDescription>
+          </div>
+          <Separator />
+        </CardHeader>
+        <CardContent>
+          <DeleteAccountForm />
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
