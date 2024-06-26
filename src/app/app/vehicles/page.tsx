0e4036bd1688ebdb1006f98031482ca319ca592c
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-26 17:45:59 -0300

    :sparkles: feat: Implementa nova tabela de veiculos

--- src/app/app/vehicles/page.tsx
+++ src/app/app/vehicles/page.tsx
@@ -1,41 +1,8 @@
 import { NextPage } from "next";
-import { DataTable } from "@/components/tables/vehicles/table";
-import { columns, type Vehicle } from "@/components/tables/vehicles/columns";
-import { Card } from "@/components/ui/card";
-
-const data: Vehicle[] = [
-  {
-    id: "1",
-    licensePlate: "BRA1234",
-    make: "Chevrolet",
-    model: "Onix",
-    status: "Ligado",
-    location: "40.730610, -73.935242",
-  },
-  {
-    id: "2",
-    licensePlate: "BRA5678",
-    make: "Volkswagen",
-    model: "Gol",
-    status: "Desligado",
-    location: "40.730610, -73.935242",
-  },
-  {
-    id: "3",
-    licensePlate: "BRA9101",
-    make: "Fiat",
-    model: "Uno",
-    status: "Ligado",
-    location: "40.730610, -73.935242",
-  },
-];
+import { DataTableVehicles } from "@/components/tables/vehicles";
 
 const VehiclesPage: NextPage = () => {
-  return (
-    <Card>
-      <DataTable columns={columns} data={data} />
-    </Card>
-  );
+  return <DataTableVehicles />;
 };
 
 export default VehiclesPage;
--- src/components/tables/vehicles/columns.tsx
+++ src/components/tables/vehicles/columns.tsx
@@ -1,57 +1,127 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import { Badge } from "@/components/ui/badge";
-import { MapPin } from "lucide-react";
-import { ActionButton } from "./action-button";
+import { ArrowUpDown, MapPin, MoreHorizontal } from "lucide-react";
 
-export type Vehicle = {
-  id: string;
-  licensePlate: string;
-  make: string;
-  model: string;
-  status: "Ligado" | "Desligado";
-  location: string;
-};
+import { type Vehicle } from "@/components/tables/vehicles";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const columns: ColumnDef<Vehicle>[] = [
   {
+    id: "Placa",
     accessorKey: "licensePlate",
     header: "Placa",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("Placa")}</div>
+    ),
   },
   {
-    accessorKey: "make",
-    header: "Marca",
-  },
-  {
-    accessorKey: "model",
-    header: "Modelo",
-  },
-  {
+    id: "Status",
     accessorKey: "status",
-    header: "Status",
-    cell: (value) => {
-      return value.getValue() === "Ligado" ? (
-        <Badge
-          className="text-green-700 font-bold bg-green-200"
-          variant="outline"
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          Ligado
-        </Badge>
-      ) : (
-        <Badge className="text-red-700 font-bold bg-red-200" variant="outline">
-          Desligado
-        </Badge>
+          Status
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
       );
     },
+    cell: ({ row }) => (
+      <div className="pl-4">
+        {row.getValue("Status") === "Em movimento" && (
+          <Badge
+            className="text-blue-700 font-bold bg-blue-200 text-nowrap"
+            variant="outline"
+          >
+            Em movimento
+          </Badge>
+        )}
+
+        {row.getValue("Status") === "Parado" && (
+          <Badge
+            className="text-yellow-700 font-bold bg-yellow-200"
+            variant="outline"
+          >
+            Parado
+          </Badge>
+        )}
+        {row.getValue("Status") === "Offline" && (
+          <Badge
+            className="text-red-700 font-bold bg-red-200"
+            variant="outline"
+          >
+            Offline
+          </Badge>
+        )}
+      </div>
+    ),
   },
   {
-    accessorKey: "location",
-    header: "Localização",
-    cell: (value) => {
-      const [lat, long] = (value.getValue() as string).split(", ");
+    id: "Marca",
+    accessorKey: "make",
+    header: ({ column }) => {
       return (
-        <span className="flex gap-2 items-center text-foreground hover:text-primary transition-colors">
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Marca
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="capitalize pl-4">{row.getValue("Marca")}</div>
+    ),
+  },
+  {
+    id: "Modelo",
+    accessorKey: "model",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Modelo
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="capitalize pl-4">{row.getValue("Modelo")}</div>
+    ),
+  },
+  {
+    id: "Localização",
+    accessorKey: "location",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Localização
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const [lat, long] = (row.getValue("Localização") as string).split(", ");
+      return (
+        <span className="pl-4 flex gap-2 items-center text-foreground hover:text-primary transition-colors">
           <MapPin className="inline-block w-4 h-4" />
           <a
             href={`https://www.google.com/maps/search/?api=1&query=${lat},${long}`}
@@ -59,17 +129,36 @@ export const columns: ColumnDef<Vehicle>[] = [
             rel="noreferrer"
             className="underline"
           >
-            {value.getValue() as string}
+            {row.getValue("Localização") as string}
           </a>
         </span>
       );
     },
   },
   {
+    id: "Ações",
     accessorKey: "actions",
-    header: "",
-    cell: ({ row }) => {
-      return <ActionButton id={row.original.id} />;
+    enableHiding: false,
+    header: "Ações",
+    cell: () => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Editar</DropdownMenuItem>
+            <DropdownMenuItem>Excluir</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Ver detalhes</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
