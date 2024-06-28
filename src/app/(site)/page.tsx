cc1de244bef36841d28fed89969c77f8e4737e61
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-27 21:30:38 -0300

    :sparkles: feat: Cria pagina principal do site

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,29 @@
+import { BenefitsSection } from "@/app/(site)/_components/benefits-section";
+import { CasesSections } from "@/app/(site)/_components/cases-seciton";
+import { CTASection } from "@/app/(site)/_components/cta-section";
+import { FAQSection } from "@/app/(site)/_components/faq-section";
+import { HeroSection } from "@/app/(site)/_components/hero/hero-section";
+import { ServicesSection } from "@/app/(site)/_components/services-section";
+import { StatsSection } from "@/app/(site)/_components/stats-section";
+import { TestimonialsSection } from "@/app/(site)/_components/testimonials-section";
+import { Separator } from "@/components/ui/separator";
+import { NextPage } from "next";
+
+const Site: NextPage = () => {
+  return (
+    <div className="overflow-x-hidden">
+      <HeroSection />
+      <ServicesSection />
+      <Separator className="bg-gradient-to-r from-transparent via-foreground to-transparent opacity-50" />
+      <StatsSection />
+      <BenefitsSection />
+      <Separator className="bg-gradient-to-r from-transparent via-foreground to-transparent opacity-50" />
+      <CasesSections />
+      <TestimonialsSection />
+      <Separator className="bg-gradient-to-r from-transparent via-foreground to-transparent opacity-50" />
+      <FAQSection />
+      <CTASection />
+    </div>
+  );
+};
+export default Site;
