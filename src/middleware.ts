f7d06078f8a5e348028e08ba316f897dd9d07ea3
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-29 16:03:08 -0300

    :sparkles: feat: Atualiza ordem das ações no callback de sucesso do login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,39 @@
 import { user } from "@/services/user";
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { routesMap } from "@/constants/routes-map";
 
+const redirectToLogin = (req: NextRequest) => {
+  const response = NextResponse.redirect(new URL(routesMap.login, req.nextUrl));
+  response.cookies.delete("app-track");
+  return response;
+};
+
+const redirectToDashboard = (req: NextRequest) => {
+  return NextResponse.redirect(new URL(routesMap.dashboard, req.nextUrl));
+};
+
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
-  const token = cookies().get("app-track");
-
-  if (!token && path.startsWith(routesMap.dashboard)) {
-    return NextResponse.redirect(new URL(routesMap.login, req.nextUrl));
+  const token = req.cookies.get("app-track");
+  console.log("token", token);
+  if (!token && path.startsWith(routesMap.app)) {
+    return redirectToLogin(req);
   }
 
   if (token) {
     try {
       const isValidUser = await user.me();
-
+      console.log("isValidUser", isValidUser);
       if (isValidUser && path.startsWith(routesMap.login)) {
-        return NextResponse.redirect(new URL(routesMap.dashboard, req.nextUrl));
+        return redirectToDashboard(req);
       }
 
-      if (!isValidUser && path.startsWith(routesMap.dashboard)) {
-        return NextResponse.redirect(new URL(routesMap.login, req.nextUrl));
+      if (!isValidUser && path.startsWith(routesMap.app)) {
+        return redirectToLogin(req);
       }
     } catch (error) {
-      const response = NextResponse.redirect(
-        new URL(routesMap.login, req.nextUrl)
-      );
-      response.cookies.delete("app-track");
-      return response;
+      console.error("Error", error);
+      redirectToLogin(req);
     }
   }
 }
