5e682659ecff9cb51fd34ffb79e7ea263cc99064
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-30 14:25:59 -0300

    :sparkles: feat: Implementa botao de voltar para home no layout de auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { BackgroundGradientAnimation } from "@/app/(auth)/_components/background-gradient-animation";
+import Link from "next/link";
+import { ArrowLeftIcon } from "lucide-react";
+import { routesMap } from "@/constants/routes-map";
+import { Button } from "@/components/ui/button";
 
 export default function AuthLayout({
   children,
@@ -9,6 +13,16 @@ export default function AuthLayout({
   return (
     <main className="min-h-dvh w-full bg-background">
       <div className="flex relative min-h-dvh w-full">
+        <Button
+          asChild
+          variant="secondary"
+          size="icon"
+          className="absolute top-4 left-4 z-50"
+        >
+          <Link href={routesMap.home}>
+            <ArrowLeftIcon />
+          </Link>
+        </Button>
         <BackgroundGradientAnimation containerClassName="absolute lg:w-1/3 lg:static"></BackgroundGradientAnimation>
         <div className="absolute h-dvh w-full bg-transparent lg:w-2/3 lg:bg-background lg:static">
           <div className="flex h-full w-full items-center justify-center px-4 py-8 lg:bg-none">
