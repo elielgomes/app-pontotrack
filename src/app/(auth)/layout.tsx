a76a2794d61b947b0a4171fec1885848511956d8
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-30 21:14:53 -0300

    :sparkles: feat: Implementa imagem de banner para layout de auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { ArrowLeftIcon } from "lucide-react";
 import { routesMap } from "@/constants/routes-map";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function AuthLayout({
   children,
@@ -23,8 +24,20 @@ export default function AuthLayout({
             <ArrowLeftIcon />
           </Link>
         </Button>
-        <BackgroundGradientAnimation containerClassName="absolute lg:w-1/3 lg:static"></BackgroundGradientAnimation>
-        <div className="absolute h-dvh w-full bg-transparent lg:w-2/3 lg:bg-background lg:static">
+        <BackgroundGradientAnimation
+          className="flex justify-center items-center h-full"
+          containerClassName="absolute lg:w-3/5 lg:static"
+        >
+          <Image
+            src="/phone-navigation.webp"
+            alt="Phone navigation"
+            width={1080}
+            height={720}
+            priority
+            className="object-cover grayscale w-full h-full opacity-25 object-center"
+          />
+        </BackgroundGradientAnimation>
+        <div className="absolute h-dvh w-full bg-transparent lg:w-2/5 lg:bg-background lg:static">
           <div className="flex h-full w-full items-center justify-center px-4 py-8 lg:bg-none">
             {children}
           </div>
